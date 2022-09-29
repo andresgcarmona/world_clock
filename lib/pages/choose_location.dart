0780bb9eeb5e32d86de75baa2ff560dc6a01e171
Andrés G. Carmona <ing.andres.carmona@gmail.com>
2022-09-29 18:25:50 -0500

    Getting data from jsonplaceholder.

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -8,6 +10,23 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    var response = await http.get(Uri.https('jsonplaceholder.typicode.com', '/todos/1'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = convert.jsonDecode(response.body);
+      print(data);
+      print(data['title']);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
